52f920d674625451469494686e00f3bfe8005f72
Blixa Morgan <blixa@projectmakeit.com>
2012-05-17 23:33:15 -0500

    Switched to canvas and getUserMedia

--- app.js
+++ app.js
@@ -29,41 +29,16 @@ app.get('/streamer/:code', function (req, res) {
 var client = io
   .of('/client')
   .on('connection', function (socket) {
-    socket.on('init client', function(){
-      if(live){
-        socket.emit('init return',{live:live,active:currentStream,streams:streamsJSON});
-      }
-    });
   });
 var controller = io
   .of('/controller')
   .on('connection', function (socket) {
-    socket.on('init', function(){
-      socket.emit('init return', {live:live,active:currentStream,streams:streamsJSON});
-    });
-    socket.on('feedcontrol', function (items) {
-      console.log("feed control :"+items.live+" - "+items.feed);
-      client.emit('feed',items);
-      controller.emit('set live', items);
-      streamsJSON[items.stream].live=items.live
-    });
-    socket.on('live select', function(stream){
-      client.emit('live select', stream);
-      controller.emit('current', stream);
-      currentStream=stream; 
-    });
   });
 var streamer = io
   .of('/streamer')
   .on('connection', function(socket){
-    socket.on('new stream', function(items){
-      streamsJSON[items.stream].address=items.address;
-      socket.emit('stream set');
-    });
-    socket.on('set live', function(items){
-      streamsJSON[items.stream].streaming=items.streaming;
-      controller.emit('stream avalible',items);
-      client.emit('stream avalible',items);
+    socket.on('data', function(data){
+      client.emit('data',data);
     });
   });
        
