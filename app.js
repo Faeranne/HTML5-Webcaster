def3c7d25c61e44b9f418030b5e2344c0760730e
Blixa Morgan <blixa@projectmakeit.com>
2012-05-18 00:23:47 -0500

    updated for dotcloud

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = require('express').createServer()
 var live=false;
 var streamsJSON = {count:1, video1:{address:""}}
 var currentStream = "video1";
-app.listen(8000);
+app.listen(8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
